080d84eda83f4bda006f77635f172b7b53d04cd9
Aryan Agrawal <aryanagrawal5@icloud.com>
2026-08-08 20:59:02 +0100

    Revert "Add development Event Maps preview"
    This reverts commit fb6842c4250d668b851cd267c68b4890dec22e7f.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -26,11 +26,6 @@ const RegressionLab = lazy(() =>
 const DataVisualiser = lazy(() =>
   import('./pages/DataVisualiser').then(m => ({ default: m.DataVisualiser })),
 )
-const EventMapsPreview = import.meta.env.DEV
-  ? lazy(() =>
-      import('./pages/EventMapsPreview').then(m => ({ default: m.EventMapsPreview })),
-    )
-  : null
 
 function RouteFallback() {
   return (
@@ -137,9 +132,6 @@ export default function App() {
           <Route path="/regression-lab" element={<RegressionLab />} />
           <Route path={CREATE_CHARTS_PATH} element={<DataVisualiser />} />
           <Route path={LEGACY_DATA_VISUALISER_PATH} element={<DataVisualiser />} />
-          {EventMapsPreview ? (
-            <Route path="/dev/event-maps" element={<EventMapsPreview />} />
-          ) : null}
         </Routes>
       </Suspense>
     </Layout>
